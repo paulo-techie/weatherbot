fac6f87a8335c7391b68333509b96290f2ddf943
stickler-ci <support@stickler-ci.com>
2020-06-18 13:35:26 +0000

    Fixing style errors.

--- lib/weather_bot.rb
+++ lib/weather_bot.rb
@@ -10,9 +10,9 @@ class WeatherBot
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         weather = Weather.new
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, " <<
-          "#{message.from.first_name}!\nWelcome to your friendly weatherbot!\n" <<
-          "#{weather.country_name} is a beautiful country.\nWe expect " <<
+        bot.api.send_message(chat_id: message.chat.id, text: 'Hello, ' \
+          "#{message.from.first_name}!\nWelcome to your friendly weatherbot!\n" \
+          "#{weather.country_name} is a beautiful country.\nWe expect " \
           "#{weather.weather_description} in #{weather.region} today ...\nGoodbye ...")
         break
       end
