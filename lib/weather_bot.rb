94878eaa8f04ec1c6c2686f1e243b0e7a1b5c6c7
paulo-techie <paulo.techie@gmail.com>
2020-06-18 05:28:18 -0600

    Correct rubocop errors|

--- lib/weather_bot.rb
+++ lib/weather_bot.rb
@@ -5,12 +5,12 @@ class WeatherBot
   def initialize
     token = '1295585207:AAFPZlHbo2WWdh9oseszmgG4qsS6Yba7yMs'
     Telegram::Bot::Client.run(token) do |bot|
-        bot.listen do |message|
-            weather = Weather.new
-            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!\n"+
-              "Welcome to your friendly weatherbot!\n#{weather.country_name} is a beautiful country.\nWe expect #{weather.weather_description} in #{weather.region} today ...\nGoodbye ...")
-            return
-        end
+      bot.listen do |message|
+        weather = Weather.new
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!\n" <<
+          "Welcome to your friendly weatherbot!\n#{weather.country_name} is a beautiful country.\nWe expect #{weather.weather_description} in #{weather.region} today ...\nGoodbye ...")
+        break
+      end
     end
   end
 end
--- lib/weather_desc.rb
+++ lib/weather_desc.rb
@@ -6,19 +6,18 @@ require 'uri'
 require 'open-uri'
 
 class Weather
-
     attr_reader :weather_description, :country_name, :region
 
     def initialize
-        @country_name =""
-        @region = ""
+        @country_name = ''
+        @region = ''
         @weather_description = make_request_to_api_endpoint
     end
 
-    private
+        private
 
     def make_request_to_api_endpoint
-        url="http://api.openweathermap.org/data/2.5/weather?#{location}&appid=2745510d199eb84e68a506d68e71467a"
+        url = "http://api.openweathermap.org/data/2.5/weather?#{location}&appid=2745510d199eb84e68a506d68e71467a"
         uri = URI(url)
         response = Net::HTTP.get(uri)
         parsed = JSON.parse(response)
@@ -28,14 +27,14 @@ class Weather
     end
 
     def location
-        url="https://reallyfreegeoip.org/json/"
+        url = 'https://reallyfreegeoip.org/json/'
         uri = URI(url)
         response = Net::HTTP.get(uri)
         parsed = JSON.parse(response)
         @country_name = (parsed['country_name']).to_s
         lat = (parsed['latitude']).to_s
-        lon = (parsed['longitude']).to_s   
+        lon = (parsed['longitude']).to_s
         "lat=#{lat}&lon=#{lon}"
     end
-      
+
 end
